ed078f28b67c62853a4ec93c478c84bad1c076cc
RocketRoss <code@radonn.co.za>
2020-11-10 13:09:29 +0000

    + writeHeader!, + writeComplex!

--- GuppiRaw.jl
+++ GuppiRaw.jl
@@ -5,54 +5,19 @@ module GuppiRaw
 	export Int4
 	export readComplex
 	export readGuppiBlock
+	export writeComplex!
+	export writeHeader!
 
-	t_GuppiHeaderDict = Dict{String, Union{Int, Float32, String}}
-
-	function parseValueString(str)
-		ret = tryparse(Int, str)
-		ret = (ret == nothing ? tryparse(Float32, str) : ret)
-		ret = (ret == nothing ? string(str) : ret)
-		ret
-	end
-
-	function readHeader!(fio)
-		headerDict = t_GuppiHeaderDict()
-		while true
-			str = string(Char.(read(fio, 80))...)
-			parts = split(str, "=")
-			if length(parts) != 2 || contains(str, "END"*" "^77)
-				break
-			end
-			headerDict[strip(parts[1])] = parseValueString(strip(parts[2]))
-		end
-		headerDict
-	end
-
-	function Int4(val::Integer)::Int8
-		return (val&0xf) > 0x7 ? 7-UInt8(val&0xf) : (val&0x7)
-	end
-
-	function readComplex!(fio, nbits)
-		if nbits == 4
-			byte = read(fio, UInt8)
-			return Int4(byte>>4) + Int4(byte)im
-		elseif nbits == 8
-			return Int8(read(fio, Int8)) + Int8(read(fio, Int8))im
-		elseif nbits == 16
-			return Int16(read(fio, Int16)) + Int16(read(fio, Int16))im
-		else
-			return nothing
-		end
-	end
+	GuppiHeaderDict = Dict{String, Union{Int, Float32, String}}
 
 	struct Guppi
 		filepath::String
-		headers::Array{t_GuppiHeaderDict}
+		headers::Array{GuppiHeaderDict}
 		blockByteOffsets::Array{Unsigned}
 	end
 	
 	function Guppi(filepath)
-		headers::Array{t_GuppiHeaderDict} = []
+		headers::Array{GuppiHeaderDict} = []
 		blockByteOffsets::Array{Unsigned} = []
 
 		open(filepath, "r") do fio
@@ -86,6 +51,62 @@ module GuppiRaw
 			seek(fio, gp.blockByteOffsets[bnum])
 			return [readComplex!(fio, nbits) for i in CartesianIndices(Tuple(dimensions))]
 		end
+	end
+
+	function parseValueString(str)
+		ret = tryparse(Int, str)
+		ret = (ret == nothing ? tryparse(Float32, str) : ret)
+		ret = (ret == nothing ? string(str) : ret)
+		ret
+	end
+
+	function readHeader!(fio::IO)
+		headerDict = GuppiHeaderDict()
+		while true
+			str = string(Char.(read(fio, 80))...)
+			parts = split(str, "=")
+			if length(parts) != 2 || contains(str, "END"*" "^77)
+				break
+			end
+			headerDict[strip(parts[1])] = parseValueString(strip(parts[2]))
+		end
+		headerDict
+	end
+
+	function Int4(val::Integer)::Int8
+		return (val&0xf) > 0x7 ? Int8(-8+(val&0x7)) : (val&0x7)
+	end
+
+	function readComplex!(fio::IO, nbits)
+		if nbits == 4
+			byte = read(fio, UInt8)
+			return Int4(byte>>4) + Int4(byte)im
+		elseif nbits == 8
+			return Int8(read(fio, Int8)) + Int8(read(fio, Int8))im
+		elseif nbits == 16
+			return Int16(read(fio, Int16)) + Int16(read(fio, Int16))im
+		else
+			return nothing
+		end
+	end
+
+	function writeComplex!(fio::IO, c::Complex, nbits)
+		if nbits == 4
+			byte = write(fio, UInt8((real(c)&0xf) << 4 + imag(c)&0xf))
+		elseif nbits == 8
+			return write(fio, Int8(real(c)&0xff), Int8(imag(c)&0xff))
+		elseif nbits == 16
+			return write(fio, Int16(real(c)&0xffff), Int16(imag(c)&0xffff))
+		else
+			return nothing
+		end
+	end
+
+	function writeHeader!(fio::IO, header::GuppiHeaderDict)
+		for kv in header
+			write(fio, rpad(kv[1]*"="*string(kv[2]), 80))
+		end
+		write(fio, rpad("END", 80))
 	end
 
 end
