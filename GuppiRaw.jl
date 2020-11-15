fb177cb88dd3236e664f1c2a5b2faae81b6cc744
RocketRoss <code@radonn.co.za>
2020-11-15 14:31:21 +0000

    ^+

--- GuppiRaw.jl
+++ GuppiRaw.jl
@@ -23,17 +23,17 @@ module GuppiRaw
 		blockByteOffsets::Array{Unsigned} = []
 
 		open(filepath, "r") do fio
-			push!(headers, readHeader!(fio))
-			blockSize = headers[1]["BLOCSIZE"] # should read per header
-			align512 = (haskey(headers[1], "DIRECTIO") ? headers[1]["DIRECTIO"] == 1 : false) # should read per header
-			@assert blockSize == GuppiRaw.calcBlockSize(headers[end])
-
 			while !eof(fio) && length(headers) < 128
+				push!(headers, readHeader!(fio))
+				blockSize = headers[1]["BLOCSIZE"]
+				align512 = (haskey(headers[1], "DIRECTIO") ? headers[1]["DIRECTIO"] == 1 : false)
+				dataSize = GuppiRaw.calcBlockSize(headers[end])
+				@assert blockSize >= dataSize "$blockSize >= $dataSize"
+
 				if align512; seekAligned!(fio, 512) end
 				push!(blockByteOffsets, position(fio))
 				skip(fio, blockSize)
 				if align512; seekAligned!(fio, 512) end
-				push!(headers, readHeader!(fio))
 			end
 			println()
 			return Guppi(filepath, headers, blockByteOffsets)
@@ -43,7 +43,6 @@ module GuppiRaw
 	function seekAligned!(fio, alignBoundary)
 		pos = position(fio)
 		offset = (pos+(alignBoundary-1)) & ~(alignBoundary-1) - pos
-		println(offset)
 		skip(fio, offset)
 	end
 	
@@ -52,9 +51,13 @@ module GuppiRaw
 		while true
 			str = string(Char.(read(fio, 80))...)
 			parts = split(str, "=")
-			if length(parts) != 2 || contains(str, "END"*" "^77)
+			if contains(str, "END"*" "^77)
 				break
 			end
+			if length(parts) != 2
+				return false
+			end
+			
 			headerDict[strip(parts[1])] = parseValueString(strip(parts[2]))
 		end
 		headerDict
@@ -62,8 +65,8 @@ module GuppiRaw
 
 	function parseValueString(str)
 		ret = tryparse(Int, str)
-		ret = (ret == nothing ? tryparse(Float32, str) : ret)
-		ret = (ret == nothing ? string(str) : ret)
+		ret = (isnothing(ret) ? tryparse(Float32, str) : ret)
+		ret = (isnothing(ret) ? string(str) : ret)
 		ret
 	end
 	
@@ -90,6 +93,10 @@ module GuppiRaw
 			"NANTS"
 		]
 
+		if all(haskey(header, key) for key in dimensionKeys) == false
+			println.(i for i in header)
+		end
+
 		dimensions = [1:header[key] for key in dimensionKeys]
 		dimensions[4] = 1:div(length(dimensions[4]), header["PKTNTIME"])
 		Tuple(dimensions)
@@ -99,26 +106,37 @@ module GuppiRaw
 		return (val&0xf) > 0x7 ? Int8(-8+(val&0x7)) : (val&0x7)
 	end
 
-	function readComplex!(fio::IO, nbits)
+	function UInt4(val::Integer)::Int8
+		return val&0xf
+	end
+
+	function readComplex!(fio::IO, nbits; unsigned = true)
 		if nbits == 4
 			byte = read(fio, UInt8)
-			return Int4(byte>>4) + Int4(byte)im #not Int4(byte<<4)im ?? No
+			return unsigned ? 
+					UInt4(byte>>4) + UInt4(byte)im :
+					Int4(byte>>4) + Int4(byte)im
 		elseif nbits == 8
-			return Int8(read(fio, Int8)) + Int8(read(fio, Int8))im
+			return unsigned ? 
+					UInt8(read(fio, Int8)) + UInt8(read(fio, Int8))im :
+					Int8(read(fio, Int8)) + Int8(read(fio, Int8))im
 		elseif nbits == 16
-			return Int16(read(fio, Int16)) + Int16(read(fio, Int16))im
+			return unsigned ? 
+					UInt16(read(fio, Int16)) + UInt16(read(fio, Int16))im :
+					Int16(read(fio, Int16)) + Int16(read(fio, Int16))im
 		else
 			return nothing
 		end
 	end
 
-	function writeComplex!(fio::IO, c::Complex, nbits)
+	function writeComplex!(fio::IO, c::Complex, nbits; cast4bit=false)
 		if nbits == 4
-			byte = write(fio, UInt8((real(c)&0xf) << 4 + imag(c)&0xf))
+			byte = write(fio, UInt8((real(c)&0xf) << 4 + (imag(c)&0xf)))
 		elseif nbits == 8
-			return write(fio, Int8(real(c)&0xff), Int8(imag(c)&0xff))
+			mask = cast4bit ? 0x0f : 0xff
+			return write(fio, UInt8(real(c)&mask), UInt8(imag(c)&mask))
 		elseif nbits == 16
-			return write(fio, Int16(real(c)&0xffff), Int16(imag(c)&0xffff))
+			return write(fio, UInt16(real(c)&0xffff), UInt16(imag(c)&0xffff))
 		else
 			return nothing
 		end
